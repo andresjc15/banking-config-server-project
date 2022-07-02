92ccf13aa6a2464b41c981ef78ca96d3f374e092
andresjc15 <andres.jcp15@gmail.com>
2022-07-02 00:34:08 -0500

    primera carga

---
+++ src/test/java/pe/com/nttdata/server/config/banking/BankingConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.nttdata.server.config.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
